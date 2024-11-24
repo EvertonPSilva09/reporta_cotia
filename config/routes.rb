155b303e44140f321e7eae180554c3911065677b
Everton Silva <everton.p.silva@outlook.com>
2024-11-24 00:45:57 -0300

    feat: add admin namespace and approve action to reports controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,16 @@ Rails.application.routes.draw do
   
   # Define routes for report index
   root to: 'reports#index'
-  resources :reports, only: [:index, :show, :new, :create, :edit, :update]
+
+  namespace :admin do
+    resources :users, only: [:index, :edit, :update]
+  end
+  
+  resources :reports, only: [:index, :show, :new, :create, :edit, :update] do
+    member do
+      patch :approve
+    end
+  end
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
 end
