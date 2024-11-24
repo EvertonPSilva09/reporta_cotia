fdccf75685dbfbef27d0bb1c8182afe029213cdd
Everton Silva <everton.p.silva@outlook.com>
2024-11-24 00:46:30 -0300

    feat: add table definitions to models

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,14 @@
+#
+# table name: reports
+# t.string :title
+# t.text :description
+# t.references :category, null: false, foreign_key: true
+# t.references :address, null: false, foreign_key: true
+# t.references :user, null: false, foreign_key: true
+# t.timestamps
+# t.index [:category_id, :address_id, :user_id], unique: true
+# t.index [:address_id, :user_id], unique: true
+# 
 class Report < ApplicationRecord
   belongs_to :category
   belongs_to :address
