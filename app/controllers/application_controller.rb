33f284406777ac7a20f91f3fe42a5f025a32156b
Everton Silva <everton.p.silva@outlook.com>
2024-11-24 00:25:28 -0300

    feat: add authorization methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: %i[index show]
+
+  def authorize_admin
+    redirect_to root_path, alert: 'Not authorized' unless current_user.admin?
+  end
+
+  def authorize_moderator
+    redirect_to root_path, alert: 'Not authorized' unless current_user.moderator? || current_user.admin?
+  end
 end
