12c5e40510250b04239f589ad4b5ef158783d463
Everton Silva <everton.p.silva@outlook.com>
2024-11-24 23:27:44 -0300

    feat: add set_locale before_action in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: %i[index show]
+  before_action :set_locale
 
   def authorize_admin
     redirect_to root_path, alert: 'Not authorized' unless current_user.admin?
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
 
   def authorize_moderator
     redirect_to root_path, alert: 'Not authorized' unless current_user.moderator? || current_user.admin?
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
