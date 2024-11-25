ff69fc9ff2001c4c1693c744a9f0e3003f52df75
Everton Silva <everton.p.silva@outlook.com>
2024-11-24 23:04:30 -0300

    feat: add status column to reports table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_24_025207) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_24_035048) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_24_025207) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.integer "status"
     t.index ["address_id"], name: "index_reports_on_address_id"
     t.index ["category_id"], name: "index_reports_on_category_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
