0cc85d5f24f601e87d3bcb2e946e3ca92c5fe38a
Everton Silva <everton.p.silva@outlook.com>
2024-12-02 22:59:52 -0300

    feat: add category_name domain and update categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_26_235209) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_02_234500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_26_235209) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
