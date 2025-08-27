897b309a74be90fb6d8acc0523238de9575d4365
Everton Silva <everton.p.silva@outlook.com>
2025-08-26 23:04:33 -0300

    fix: add missing search route for reports

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,21 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Define routes for report index
   root to: 'reports#index'
 
   namespace :admin do
     resources :users, only: [:index, :edit, :update]
   end
-  
+
   resources :reports, only: [:index, :show, :new, :create, :edit, :update] do
     member do
       patch :approve
     end
     collection do
       get :pending
+    get :search
     end
   end
 
