3d11c35baf3f1b357d691422240358adf4ef28f6
Everton Silva <everton.p.silva@outlook.com>
2025-08-26 22:38:47 -0300

    feat: add user_roles table and update users table to reference role_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_02_234500) do
+ActiveRecord::Schema[7.0].define(version: 2025_08_26_121000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_02_234500) do
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
+  create_table "user_roles", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_user_roles_on_name", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -88,9 +95,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_02_234500) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
+    t.bigint "role_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
@@ -98,4 +106,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_02_234500) do
   add_foreign_key "reports", "addresses"
   add_foreign_key "reports", "categories"
   add_foreign_key "reports", "users"
+  add_foreign_key "users", "user_roles", column: "role_id"
 end
